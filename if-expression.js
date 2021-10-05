9864bfc6e3279645128174d0fc58342ebeab09bc
kadekframa <dframa19@gmail.com>
2021-10-05 18:59:09 +0800

    belajar If Exression Javascript. Tetap Semangat git add .!

---
+++ if-expression.js
@@ -0,0 +1,15 @@
+
+const examValue =  100;
+
+    // If Expression lebih dari 2 logic:
+    if( examValue > 80){
+        document.writeln("Good Job, Tetap semangat!!!");
+    }else if(examValue > 70){
+        document.writeln("Tetap Semangat Berjuang!!!");
+    }else if(examValue > 60){
+        document.writeln("Mantap djiwa, tetap semangat !!!");
+    }else{
+        document.writeln("Tetap berjuang !!!");
+    };
+
+    //SEMANGAT !!!
