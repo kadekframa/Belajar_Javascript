b0e1b092172b90d29c8da7dd69477d9a15b79137
kadekframa <dframa19@gmail.com>
2021-10-06 23:39:44 +0800

    belajar operator typeof Javascript, Semangat!

---
+++ operator-typeof.js
@@ -0,0 +1,7 @@
+// typeof merupakan tipe data yang digunakan untuk melihat tipe data sebuah value atau variable.
+// hasil dari operator typeof adalah string tipe datanya.
+
+let data = "null";
+
+const typeData = typeof data;
+document.writeln(`<p>${typeData}</p>`);
