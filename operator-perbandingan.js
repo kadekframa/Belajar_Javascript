f8ed442e165dc049e171fc4af8a75054b2ede375
kadekframa <dframa19@gmail.com>
2021-10-06 17:35:30 +0800

    modifikasi operator perbandingan, Semangat!

---
+++ operator-perbandingan.js
@@ -0,0 +1,8 @@
+let result = 5 == "5";
+            document.writeln("<p>" + result + "</p>");
+
+            result = 5 === "5";
+            document.writeln("<p>" + result + "</p>");
+
+            result = 5 < 10; //false
+            document.writeln("<p>" + result + "</p>");
