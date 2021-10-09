02dcb7bea6e0f124875cb5ba39c78dca590f56e3
kadekframa <dframa19@gmail.com>
2021-10-09 22:02:42 +0800

    belajar perulangan while JavaScript, Tetap Semangat!

---
+++ while-loop.js
@@ -0,0 +1,9 @@
+// while loop adalah versi perulangan yang lebih sederhana dibanding for loop.
+// di while loop, hanya terdapat kondisi perulangan, tanpa init statement dan post statement.
+
+// contoh penggunaan while loop
+let counter = 2;
+while(counter < 8){
+    document.writeln(`<p>Perulangan yang ke ${counter}</p>`);
+    counter++;
+};
