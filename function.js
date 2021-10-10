c0cd7adcf1057d8f9c7e71a47d9f0572d03db7df
kadekframa <dframa19@gmail.com>
2021-10-10 23:51:21 +0800

    belajar function JavaScript, Semangat!

---
+++ function.js
@@ -0,0 +1,14 @@
+// function adalah blok kode program saat kita panggil.
+// kita bisa membuat kode program, dimana kita bisa simpan dalam sebuah function. Nanti ketika kita membutuhkannya, kita dapat memanggil function tersebut.
+// untuk membuat function, kita bisa menggunakan kata kunci function, lalu diikuti dengan nama function, kurung() dan diakhiri dengan block kurung{}.
+// kita bisa memanggil function dengan nama function lalu diikuti dengan kurung ().
+// Di bahasa pemrograman lain, function juga disebut dengan method.
+
+//membuat function
+function sayHelloWorld(){
+    document.writeln(`<p>Hello World</p>`);
+}
+
+//memanggil function
+sayHelloWorld();
+sayHelloWorld(); // boleh berkali-kali
