527257cf8b47190fe2b514deaeb6c26676e57676
kadekframa <dframa19@gmail.com>
2021-10-10 08:36:08 +0800

    belajar fitur label pada JavaScrript, Tetap Semangat!

---
+++ label.js
@@ -0,0 +1,13 @@
+// Label merupakan penanda yang bisa digunakan dengan kata kunci break dan continue.
+// 
+
+
+// contoh penggunaan label.
+// contoh 1.
+loopi:
+for(let i=0 ; i<6; i++){
+    loopj:
+    for(let j=0 ; j<10; j++){
+        console.info(`${i} - ${j}`);
+    }
+};
