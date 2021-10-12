f7d3308f21a1a00fa2b99a9c289eb87d688210ec
kadekframa <dframa19@gmail.com>
2021-10-12 22:23:55 +0800

    belajar optional parameter JavaScript, Tetap Semangat! ! !

---
+++ optional-parameter.js
@@ -0,0 +1,12 @@
+// Optional parameter.
+// Di javascript semua parameter nya itu optional.
+
+function sayHello(firstName, middleName, lastName){
+    console.log(firstName);
+    console.log(middleName);
+    console.log(lastName);
+};
+
+sayHello("Kadek");
+sayHello("Kadek");
+sayHello("Kadek", "Frama");
