b75b9721e030667e347b2afc1cace9cb848e3942
kadekframa <dframa19@gmail.com>
2021-10-14 23:54:04 +0800

    belajar scope pada JavaScript, Semangat!

---
+++ scope.js
@@ -0,0 +1,6 @@
+// Scope merupakan area akses sebuah data.
+// Ada dua jenis scope, global scopoe dan local scope.
+// Setiap kita membuat function, maka kita akan membuat local scope untuk function tersebut.
+// Data di global scope bisa diakses dari local scope, namun data di local scope hanya bisa di akses di local scope tersebut atau di scope local dibawahnya (dalam kasus function dalam fucntion).
+
+// Contoh penggunaan scope.
