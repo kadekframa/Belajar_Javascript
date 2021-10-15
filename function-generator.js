553253742e193b34d9ba2eff54ed0d0a3da887af
kadekframa <dframa19@gmail.com>
2021-10-15 11:05:35 +0800

    belajar penggunaan function generator pada JavaScript, Tetap Semangat!

---
+++ function-generator.js
@@ -0,0 +1,6 @@
+// Function generator adalah function yang digunakan untk membuat data generator.
+// Generator adalah data yang bisa di integrasi seperti Array.
+// Untuk membuat function generator, kita perlu menggunakan tanda * (bintang) setelah kata function.
+// dan untuk mengembalikan data di tiap iterasi, kita bisa gunakan kata kunci yield diikuti datanya.
+
+// Contoh penggunaan function generator.
