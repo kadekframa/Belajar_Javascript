8cf110895ba57074eb95489ec25e78e1677ba601
kadekframa <dframa19@gmail.com>
2021-10-15 22:55:05 +0800

    belajar arrow function pada JavaScript, Tetap Semangat!

---
+++ arrow-fucntion.js
@@ -0,0 +1,17 @@
+// Arrow Function adalah alternatif pembuatan function yang lebih sederhana dari function biasanya.
+// Namun terdapat limitasi dan juga tidak bisa digunakan di semua situasi.
+// Dinamakan Arrow function karena menggunakan tana => (seperti anak panah).
+// Berikut contoh beberapa kekurangan arrow function:
+/*  - tidak memiliki fitur arguments object.
+    - tidak bisa menggunakan function generator.
+    - tidak bisa mengakses this (yag nanti akan dibahas di function di obejct).
+    - tidak bisa mengakses super (yang nant akan dibahas di JavaScript OOP).
+*/
+
+// contoh penggunaa arrow function.
+const sayHello = (name) => {
+    const say = `Hello ${name}`;
+    console.info(say);
+}
+
+sayHello("Kadek Frama");
