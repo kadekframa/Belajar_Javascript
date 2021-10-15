bd0295fd8c09d7b94ea95231d06d5bc97dbd0c2d
kadekframa <dframa19@gmail.com>
2021-10-15 15:23:08 +0800

    belajar contoh penggunaan function generator pada JavaScript, Tetap Semangat!

--- function-generator.js
+++ function-generator.js
@@ -17,3 +17,19 @@ for (const name of names) {
     document.writeln(name);
 }
 
+
+
+// contoh penggunaan function generator yang kompleks.
+function* buatGanjil(value){
+    for(let i = 1; i <= value; i++){
+        if(i % 2 === 1){
+            yield i;
+        }
+
+    }
+}
+
+const numbers = buatGanjil(100);
+for (const number of numbers) {
+    console.info(number);
+}
