b1a1b8d8dd51541b39037e93c0dd6772508a8bea
kadekframa <dframa19@gmail.com>
2021-10-15 10:58:24 +0800

    belajar penggunaan recursive function pada JavaScript, Tetap Semangat!

--- recursive-function.js
+++ recursive-function.js
@@ -2,3 +2,27 @@
 // Kadang memang ada banyak problem, yang lebih mudah diselesaikan menggunakan recursive function, seperti contohnya kasus faktorial.
 
 // contoh penggunaan recursive function.
+
+// faktorial yang menggunakan perulangan biasa.
+function factorial(value){
+    let result = 1;
+    for(let i = 1; i <= value; i++){
+        result *= i;
+    }
+    return result;
+};
+
+console.info(factorial(5));
+
+
+// faktorial yang menggunakan perulangan rekursive. Menggunakan perulangan rekursive akan terlihat lebih sederhana, namun perlu dipelajari lagi untuk dapat membiasakan penggunaannya.
+function factorialRecursive(value){
+    if(value === 1){
+        return 1;
+    }else{
+        return value * factorialRecursive(value - 1);
+    }
+};
+
+console.info(factorialRecursive(10));
+document.writeln(factorialRecursive(5));
