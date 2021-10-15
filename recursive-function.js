ed9691c5bad27ec1e46a151fcacf956c37a5ea3c
kadekframa <dframa19@gmail.com>
2021-10-15 10:30:37 +0800

    belajar penggunaan recursive function pada JavaScript, Tetap Semangat!

---
+++ recursive-function.js
@@ -0,0 +1,4 @@
+// recursive function adalah kemapuan function memanggil function dirinya sendiri.
+// Kadang memang ada banyak problem, yang lebih mudah diselesaikan menggunakan recursive function, seperti contohnya kasus faktorial.
+
+// contoh penggunaan recursive function.
