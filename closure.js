3e0089ddb40054f0d4b65071c7d3e551f8431b14
kadekframa <dframa19@gmail.com>
2021-10-25 19:11:57 +0800

    belajar closure di JavaScript, Tetap Semangat!

---
+++ closure.js
@@ -0,0 +1,7 @@
+// Closure di JavaScript.
+// Closure adalah kombinasi funcition dan bundel referensi ke data disekitarnya.
+// Kita sudah tahu bahwa local scope tidak bisa diakses di luar scopenya.
+// Dengan kemampuan closurem kita bisa membuat sebuah function di local scope dan referensi ke data di sekitar local scope tersebut, keluar scope nya.
+
+//Contoh penggunaan closure di JavaScript.
+
