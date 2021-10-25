09b2d712a3e7a6645128cbc5e82ed5e750b094bf
kadekframa <dframa19@gmail.com>
2021-10-25 17:16:34 +0800

    belajar penggunaan arrow function di JavaScript, Tetap Semangat!!!

--- arrow-fucntion.js
+++ arrow-fucntion.js
@@ -9,12 +9,47 @@
 */
 
 // contoh sederhana penggunaan arrow function.
-const sayHello = (name) => {
+// Arrow Function sebagai variable.
+
+const sayHello = (name) => {                    // membuat arrow function. Arrow function tidak dapat berdiri sendiri, maka dari itu harus disimpan di dalam variable atau parameter.
     const say = `Hello ${name}`;
     console.info(say);
 }
 
-sayHello("Kadek Frama");
+sayHello("Kadek Frama");                        // memanggil arrow function yang berisikan value parameter.
 
 
-// 
+// Contoh arrow function satu line. Jika kode yang ada di dalam block arrow function hanya satu line saja, itu dapat dibuat sebagai berikut.
+const sayCongrats = (ucapan) => document.writeln(`Hallo Kadek Frama ${ucapan}`); // Contoh kode penggunaan arrow function sederhana dengan satu line saja.
+sayCongrats("Selamat dan Tetap Semangat...!!!");        // memanggil arrow function yang berisikan value parameter.
+
+
+// Contoh arrow function. Arrow function juga dapat mengembalikan value atau return value
+const sum = (first, second) => first + second;                      // Contoh arrow function tanpa block (jika kode di dalam block hanya satu line saja).
+
+// arrow function dapat dibuat seperti diatas atau seperti dibawah
+
+// const sum = (firt, second) => {                                  // Contoh arrow function dengan block.
+//     return firt + second;
+// };
+
+sumPlus = sum(2, 3);                                                // Menyimpan sekaligus memanggil arrow function ke dalam variable sumPlus.
+document.writeln("<p><br></p>");
+document.writeln(`Total Penjumlahannya Adalah : <b> ${sumPlus}</b>`);   // Menampilkan value dari arrow function.
+
+
+
+//  Arrow function sebagai parameter.
+function giveMeName(callback){
+    callback("Kadek Frama");        // mengisi value dari parameter di arrow function.
+};
+
+document.writeln("<p><br></p>");
+giveMeName((nama) => document.writeln(`Hallo ${nama}, ini adalah contoh penggunaan arrow function sebagai parameter yaa..`));     // penggunaan arrow function sebagai parameter.
+
+
+
+
+
+// Kesimpulan: Arrow Function itu mirip seperti anonymous function, karena sama-sama tidak dapat berdiri sendiri. Arrow function harus disimpan sebagai variable atau sebagai parameter seperti pada contoh diatas.
+// Tetap Semangat berlatih JavaScript..!!! 
