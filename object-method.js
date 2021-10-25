edd8bac8adbbf6816463e8f0fd4cb9c9c434a709
kadekframa <dframa19@gmail.com>
2021-10-25 22:49:28 +0800

    belajar penggunaan object method di JavaScript, Tetap Semangat!

--- object-method.js
+++ object-method.js
@@ -1,2 +1,7 @@
 // Object Method di JavaScript.
-//
+// Pada tipe data object, kita sudah membahas tentang property di object.
+// Karena sebenarnya function juga merupakan salah satu tipe data, jadi function pun sebenarnya bisa ditambahkan sebagai property di object.
+// Cara pembuatannya pun sama dengan function sebagai value.
+// Function di object kadang disebut juga sebagai Object Method.
+
+// Contoh penggunaan object method.
