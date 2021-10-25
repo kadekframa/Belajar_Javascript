caf6479b8550878be3112abbc7de996841fbc16d
kadekframa <dframa19@gmail.com>
2021-10-26 01:50:43 +0800

    belajar penggunaan kata kunci this di JavaScript, Tetap Semangat Meraih Impian!

--- this.js
+++ this.js
@@ -7,3 +7,36 @@
 // Dalam event, this merupakan referensi ke element yang menerima event (dibahas di materi Document Object Model).
 
 // Contoh peggunaan kata kunci this di JavaScript.
+
+// this di global scope.
+console.info(this);         // window
+
+// this di function scope.
+function sample(){
+    console.info(this);     // window
+};
+
+sample();
+
+// this di nested function.
+function sample2(){
+    function nested(){
+        console.info(this); // window
+    }
+
+    nested();
+}
+
+sample2();
+
+
+
+// this di object method.
+const person = {
+    name: "Kadek",
+    sayHello: function (value){
+        console.info(`Hello ${value} My Name is ${this.name}`); // di object, this merupakan referensi ke pemiliki function atau object method nya ( dalam hal ini object nya adalah person).
+    }
+};
+
+person.sayHello("Kadek Frama");     // memanggil object method di object person.
