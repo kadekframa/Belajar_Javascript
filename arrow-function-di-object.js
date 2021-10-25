0c0a58bccf6d64cfd13f3f1d5d5f3f0c34298dd5
kadekframa <dframa19@gmail.com>
2021-10-26 02:07:48 +0800

    belajar penggunaan arrow function di object pada JavaScript, Tetap Semangat berlatih JavaScript!

--- arrow-function-di-object.js
+++ arrow-function-di-object.js
@@ -5,3 +5,14 @@
 // Jadi pastikan gunakan arrow function hanya memang ketika kita tidak butuh fitur-fitur diatas.
 
 // Contoh penggunaan arrow function di object.
+const person = {
+    name: "Kadek",
+    sayHello: (nama) => {
+        console.info(`Hello ${nama}`);      // arrow function bisa diterapkan di object method, namun arrow function pada object method tidak dapat mengakses kata kunci "this".
+    }
+};
+
+person.sayHello("Kadek Frama");             // memanggil object method sayHello() yang berisikan value parameter. Dalam hal ini object method sayHello() itu berisikan arrow function yang diterapkan pada object atau disebut object method.
+
+
+// Tetap Semangat Berlatih JavaScript!, Pantang Menyerah demi meraih impian! :)
