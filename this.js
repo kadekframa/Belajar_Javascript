3b55a3b88129f46a3f50b77a8016d3beea0f9e9a
kadekframa <dframa19@gmail.com>
2021-10-26 01:32:34 +0800

    belajar penggunaan kata kunci this di JavaScript, Tetap Semangat!

---
+++ this.js
@@ -0,0 +1,9 @@
+// Kata kunci this di JavaScript.
+// Kata kunci this adalah referensi ke object milik siapa.
+// Dalam object method, this merupakan referensi ke object pemilik function nya.
+// Di global scope, this merupakan referensi ke global object (di browser biasanya window).
+// Dalam function, this merupakan referensi ke global object (di browser biasanya window).
+// Di function dengan strict mode (akan dibahas nanti), this adalah undefined.
+// Dalam event, this merupakan referensi ke element yang menerima event (dibahas di materi Document Object Model).
+
+// Contoh peggunaan kata kunci this di JavaScript.
