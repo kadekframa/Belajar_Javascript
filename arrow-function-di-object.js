1b5da2954c6136b1dffbfd59f3d8435264315882
kadekframa <dframa19@gmail.com>
2021-10-26 01:57:51 +0800

    belajar penggunaan arrow function di object pada JavaScript, Tetap Semangat berlatih!

---
+++ arrow-function-di-object.js
@@ -0,0 +1,7 @@
+// Arrow function di JavaScript.
+// Sebelumnya kita sudah bahas tentang arrow function.
+// Arrow function juga bisa kita gunakan di sebagai object method.
+// Namun perlu diketahui, arrow function tidak bisa digunakan untuk mengakses arguments object, function generator, kata kunci this dan kata kunci super (dibahas di materi JavaScript Object Oriented Programming).
+// Jadi pastikan gunakan arrow function hanya memang ketika kita tidak butuh fitur-fitur diatas.
+
+// Contoh penggunaan arrow function di object.
