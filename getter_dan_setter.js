308cd8dc2a4462af3e89caec7554457e7955adfa
kadekframa <dframa19@gmail.com>
2021-11-29 23:59:18 +0800

    Belajar penggunaan getter dan setter di di Javacript, Tetap semangat berlatih Javascript, semangat meraih impian!

--- getter_dan_setter.js
+++ getter_dan_setter.js
@@ -1,3 +1,25 @@
 
 // Penggunaan Getter dan Setter di Javascript.
+// Getter dan Setter adlaah kemampuan membuat function yang berbeda untuk mengambil data (Getter) dan mengubah data (Setter) oada sebuah property di Object.
+// Dengan menggunakan getter dan setter, kita bisa melakukan hal apapun dalam function sebelum sebuah property di akses atau diubah, misal menambah validasi dan lain-lain.
+
+const person = {
+    firstName: "Kadek",
+    lastName: "Frama",
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`;
+    },
+    set fullName(value){
+        const array = value.split(" ");
+        this.firstName = array[0];
+        this.lastName = array[1];
+    }
+};
+
+
+console.info(person.fullName);  // memanggil getter fullName;
+
+person.fullName = "Budi Utomo"; // mengakses setter fullName, dengan tujuan untuk mengubah value dari property fullName menjadi "Budi Utomo";
+console.table(person);          // mengakses object person dengan menggunaknan console table.
+
 
