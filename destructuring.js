100a8c2faf2ff4a334d510f8bf8426a5d85d7b07
kadekframa <dframa19@gmail.com>
2021-11-30 21:54:35 +0800

    Javascript learning by doing, Berlatih penggunaan destructuring pada Javascript. Tetap semangat berlatih Javascript!

--- destructuring.js
+++ destructuring.js
@@ -3,5 +3,24 @@
 // Destructuring adalah fitur yang bisa digunakan untuk membongkar value-value di array atau object ke dalam sebuah variable-variable baru.
 // Fitur ini sangat mempermudah kita ketika ingin mengambil data dari array atau object tanpa harus melakukan pengambilan data satu persatu.
 
-// Contoh penerapan mengambil data di array.
+// Contoh mengambil data di array tanpa destructuring.
+const names = ["Kadek", "Frama", "Danamastyana"];
+const firstName = names[0];
+const middleName = names[1];
+const lastName = names[2];
 
+console.info(firstName);
+console.info(middleName);
+console.info(lastName);
+console.info("");
+
+
+
+// Contoh mengambil data di array dengan menerapkan destructuring (destructuring array).
+const namaNya = ["Budi", "Doremi", "Prakarsa", "Budi", "Tetap semangat"];
+const [namaAwal, namaTengah, namaAkhir, ...others] = namaNya;
+
+console.info(namaAwal);
+console.info(namaTengah);
+console.info(namaAkhir);
+console.info(others);
