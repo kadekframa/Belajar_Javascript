1551beaa6c85ee21a1b5439b6a610244da0a6e16
kadekframa <dframa19@gmail.com>
2022-01-17 00:37:40 +0800

    Berlatih JavaScript, repeat cara penggunaan optional chainning pada JavaScript. Tetap Semangat Berlatih!!!

---
+++ repeat-optional-chainning.js
@@ -0,0 +1,6 @@
+// Optional Chainning pada JavaScript.
+
+// Optional chainning operator (?) merupakan operator yang digunakan untuk mengamankan ketika kita ingin mengakses property sebuah object dari data nullish.
+// Jika kita mencoba mengakses property dari sebuah object dari data nullish tanpa menggunakan optional chainning operator, maka akan terjadi error.
+
+
