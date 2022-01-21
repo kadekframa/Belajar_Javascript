21d45dfc2f5cbfb5f3b0fb2f9c6be23d6e187945
kadekframa <dframa19@gmail.com>
2022-01-21 21:11:10 +0800

    Test node in JavaScript, Tetap Semangat!!!

--- repeat-optional-chainning.js
+++ repeat-optional-chainning.js
@@ -4,3 +4,6 @@
 // Jika kita mencoba mengakses property dari sebuah object dari data nullish tanpa menggunakan optional chainning operator, maka akan terjadi error.
 
 
+let variable = "Kadek";
+
+console.log(`Hello ${variable}, it's nice to meet you and learn programming with you`);
