c022eeac681d19d649cf355791d9a84c60fbe493
kadekframa <dframa19@gmail.com>
2022-01-23 10:18:59 +0800

    Berlatih JavaScript, Repeat falsy and truthy in JavaScript. Kembali menjadi lebih kuat!!!

---
+++ repeat-falsy-truthy.js
@@ -0,0 +1,20 @@
+// Falsy dan Truthy pada JavaScript.
+
+// Data falsy (Semua data seperti di bawah ini akan dianggap falsy di JavaScript):
+/**
+ *  - false
+ *  - 0, -0
+ *  - "", '', ``
+ *  - null
+ *  - undefined
+ *  - NaN
+ */
+
+
+let data = [];
+
+if(data){
+    document.writeln("TRUE");
+}else{
+    document.writeln("FALSE");
+}
