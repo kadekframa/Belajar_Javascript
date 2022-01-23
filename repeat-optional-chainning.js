83931c82ee7f7b118592de548dae520e26117c9b
kadekframa <dframa19@gmail.com>
2022-01-23 09:53:38 +0800

    Berlatih JavaScript, Repeat optional chainning in JavaScript. Kembali menjadi lebih kuat!!!

--- repeat-optional-chainning.js
+++ repeat-optional-chainning.js
@@ -4,6 +4,28 @@
 // Jika kita mencoba mengakses property dari sebuah object dari data nullish tanpa menggunakan optional chainning operator, maka akan terjadi error.
 
 
-let variable = "Kadek";
 
-console.log(`Hello ${variable}, it's nice to meet you and learn programming with you`);
+const person = {
+    
+};
+
+const country = person?.address?.country;
+
+
+// let say;
+// if(person.address !== undefined && person.address !== null){        // cara manual untuk mengamankan ketika kita ingin mengakses property sebuah object dari data undefined dan nullish.
+//     say = person.address.country;
+// }
+
+// alert(`Negara: ${say}`);
+
+
+// alert(`Ini adalah contoh penerapan optional chainning: ${person?.address?.country}`);    // contoh penerapan optional chainning untuk mengamankan ketika kita ingin mengakses property sebuah object dari data undefined dan nullish.
+
+document.writeln(`<p> ${country} </p>`);
+document.writeln(`<p> SUKSES`);
+
+
+
+
+// NB: dengan menggunakan optional chainning ini, kode akan terlihat lebih simple dan mudah digunakan.
