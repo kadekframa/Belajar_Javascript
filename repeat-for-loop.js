6864f03fe7bd14c43e8dd3bacc2395ae85d1f1eb
kadekframa <dframa19@gmail.com>
2022-01-24 12:19:35 +0800

    Berlatih JavaScript, Repeat Penggunaan for loop di JavaScript. Kembali menjadi lebih Kuat !!!

---
+++ repeat-for-loop.js
@@ -0,0 +1,16 @@
+// for loop pada JavaScript
+
+//
+
+
+for(i=1; i<=3; i++){
+    if(i <= 3){
+        alert(i);
+    }else{
+        break;
+    }
+    
+}
+
+
+// NB: Perulangan for loop akan banyak dipergunakan dalam pengembangan project web/mobile. Tetap Semangat Berlatih !!!
