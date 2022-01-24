5f367dfcfdd12dbbc9a5d5c3938f87222e220a00
kadekframa <dframa19@gmail.com>
2022-01-24 12:27:39 +0800

    Berlatih JavaScript, repeat penggunaan while loop pada JavaScript, Tetap semangat berlatih !!!, Kembali menjadi lebih Kuat!!!

---
+++ repeat-while-loop.js
@@ -0,0 +1,17 @@
+// While loop pada JavaScript.
+
+// while loop adalah versi perulangan yang lebih sederhana dibanding dengan for loop.
+// di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement.
+
+
+// Contoh penggunaan while loop.
+let counter = 1;
+while(counter <= 10){
+    document.writeln(`<p>Perulangan ke ${counter}</p>`);
+    counter++
+}
+
+
+
+// NB: Apabila dalam melakukan perulangan kita hanya membutuhkan kondisi nya saja, maka disarankan kita tidak perlu menggunakan for loop namun dapat menggunakan while loop.
+// TETAP SEMANGAT BERLATIH !!!
