7e571defd5b73ac87eb6df1c0a3d0465cf306530
kadekframa <dframa19@gmail.com>
2022-02-05 17:26:00 +0800

    Berlatih javascript, repeat penggunaan function sebagai value dan function sebagai parameter. Tetap Semangat !!!, Kembali menjadi lebih Kuat !!!, Semangat!!!

--- repeat-function-sebagai-value.js
+++ repeat-function-sebagai-value.js
@@ -9,6 +9,7 @@ function sayHello(name){
     document.writeln(`<p>Hello ${name}</p>`);
 }
 
+// Contoh penggunaan function sebagai value.
 let say = sayHello;                 // memasukkan function sayHello() ke dalam variable say (function sebagai value).
 
 
@@ -16,5 +17,13 @@ sayHello(`Kadek Frama`);            // memanggil function sayHello() dapat mengg
 say("Kadek Frama Danamastyana");    // atau juga dapat dilakukan dengan menggunakan cara seperti ini. Karena function sayHello sebelumnya juga sudah dimasukkan kedalam variable say.
 
 
+// Function sebagai parameter.
+function giveMeName(callback){
+    callback(`Kadek Frama Callback`);
+}
 
-// NB: Tetap Semangat Berlatih !!!, Kembali menjadi lebih kuat !!!, Semangat!!!
+giveMeName(sayHello);               // memanggil function giveMeName() dengan paraemter function sayHello().
+giveMeName(say);                    // memanggil function giveMeName() dengan parameter variabel berisi value function sayHello().
+
+
+// NB: Jadi Function di JavaScript bisa dijadikan sebagai value (dimasukkan ke variable) dan bisa juga dikirim untuk menjadi parameter pada function lain. Tetap Semangat Berlatih !!!, Kembali menjadi lebih kuat !!!, Semangat!!!
