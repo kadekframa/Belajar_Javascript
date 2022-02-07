901588ff52f7c577cb49939bd37956fb83d4ea31
kadekframa <dframa19@gmail.com>
2022-02-07 22:32:33 +0800

    Berlatih JavaScript, repeat penggunaan arrow function pada JavaScript. Tetap Semangat !!!, Kembali menjadi lebih Kuat !!!

---
+++ repeat-arrow-function.js
@@ -0,0 +1,13 @@
+// Arrow Function in JavaScript.
+
+// Arrow function adalah alternatif pembuatan function yang lebih sederhana dari function biasanya.
+// Namun terdapat limitasi dan juga tidak bisa digunakan di semua situasi.
+// Dinamakan arrow function karena menggunakan tanda => (seperti panah).
+// Berikut contoh beberapa kekurangan arrow function:
+/*
+    - tidak memiliki fitur arguments object.
+    - tidak bisa menggunakan function generator.
+    - tidak bisa mengakses this (yang nanti akan dibahas di function di object).
+    - tidak bisa mengakses super (akan dibahas di JavaScript object oriented programming).
+*/
+
